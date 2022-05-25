9bf47fc3d0945c460d4197434ae889ab0e19e813
jaimeglez <jaime01glez@gmail.com>
2022-05-25 20:01:11 +0200

    master

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <nuxt-content :document="page" />
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content }) {
+    const page = await $content('home').fetch()
+    return {
+      page
+    }
+  }
+}
+</script>
